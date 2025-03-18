1f6e1fae653ada3a9828ec00ade2358f7b7c7b0e
shine85 <73932625+shine85@users.noreply.github.com>
2025-03-18 11:48:11 +0800

    更新 diy-part1-mhm.sh

--- diy-part1-mhm.sh
+++ diy-part1-mhm.sh
@@ -17,5 +17,5 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >>feeds.conf.default
+
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >>feeds.conf.default
