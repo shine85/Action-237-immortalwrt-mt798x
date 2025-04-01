5bc9cfb062737e2049bec489f3ca68cd2ba8b9ff
shine85 <73932625+shine85@users.noreply.github.com>
2025-04-01 11:35:23 +0800

    更新 diy-part2-798x.sh

--- diy-part2-798x.sh
+++ diy-part2-798x.sh
@@ -103,16 +103,17 @@ echo "CONFIG_ROOTFS_SIZE=\"$rootfs_size\"" >> .config
 echo "CONFIG_KERNEL_USAGE=\"$kernel_usage\"" >> .config
 
 # 修改插件名字
-sed -i 's/"终端"/"终端TTYD"/g' `egrep "终端" -rl ./`
-sed -i 's/"网络存储"/"NAS"/g' `egrep "网络存储" -rl ./`
-sed -i 's/"实时流量监测"/"流量"/g' `egrep "实时流量监测" -rl ./`
-sed -i 's/"KMS 服务器"/"KMS激活"/g' `egrep "KMS 服务器" -rl ./`
-sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
-sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
-sed -i 's/"管理权"/"管理权"/g' `egrep "管理权" -rl ./`
-sed -i 's/"带宽监控"/"带宽监控"/g' `egrep "带宽监控" -rl ./`
+find ./ -type f -exec sed -i 's/"终端"/"终端TTYD"/g' {} +
+find ./ -type f -exec sed -i 's/"网络存储"/"NAS"/g' {} +
+find ./ -type f -exec sed -i 's/"实时流量监测"/"流量"/g' {} +
+find ./ -type f -exec sed -i 's/"KMS 服务器"/"KMS激活"/g' {} +
+find ./ -type f -exec sed -i 's/"USB 打印服务器"/"打印服务"/g' {} +
+find ./ -type f -exec sed -i 's/"Web 管理"/"Web管理"/g' {} +
+find ./ -type f -exec sed -i 's/"管理权"/"管理权"/g' {} +
+find ./ -type f -exec sed -i 's/"带宽监控"/"带宽监控"/g' {} +
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
+CLEAR_PATH="./clear_list.txt"
 cat >"$CLEAR_PATH" <<-EOF
 packages
 config.buildinfo
@@ -126,5 +127,6 @@ openwrt-x86-64-generic-squashfs-rootfs.img.gz
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
-cat >>$DELETE <<-EOF
+DELETE="./delete_list.txt"
+cat >>"$DELETE" <<-EOF
 EOF
