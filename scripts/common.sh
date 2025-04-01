33dfac42555dbb751bb60f5459525e91e15ed05c
shine85 <73932625+shine85@users.noreply.github.com>
2025-04-01 15:03:33 +0800

    --

--- scripts/common.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# common.sh - General utility script
-# Purpose: Load Diy_xinxi.sh from the scripts directory
-
-# Use the absolute path provided by GitHub Actions environment
-# $GITHUB_WORKSPACE is the repository root
-source "$GITHUB_WORKSPACE/scripts/Diy_xinxi.sh"
