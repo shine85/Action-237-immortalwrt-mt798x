4b20cbfbfa261e3173af57b4bc60f738c9fd0b63
shine85 <73932625+shine85@users.noreply.github.com>
2025-04-08 16:15:00 +0800

    更新 diy-part2-798x.sh

--- diy-part2-798x.sh
+++ diy-part2-798x.sh
@@ -78,8 +78,9 @@ sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
 sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 
 
-# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间(根据编译机型变化,自行调整删除名称)
-cat >"$CLEAR_PATH" <<-EOF
+# 清理不需要的文件
+CLEAR_PATH="./clear_list.txt"
+cat > "$CLEAR_PATH" << EOF
 packages
 config.buildinfo
 feeds.buildinfo
