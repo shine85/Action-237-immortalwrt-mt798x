f943682dc672001097d42f8da32dacd568eaeba0
shine85 <73932625+shine85@users.noreply.github.com>
2025-04-15 16:21:31 +0800

    更新 diy-part2-798x.sh

--- diy-part2-798x.sh
+++ diy-part2-798x.sh
@@ -1,82 +1,146 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展二合一了，在此处可以增加插件
-# 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
-# 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+# 克隆插件
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki package/Nikki
+git clone https://github.com/281677160/luci-app-autoupdate package/autoupdate
+
+# 创建 UCI 默认配置文件目录
+mkdir -p package/base-files/files/etc/uci-defaults
 
 # 后台IP设置
-export Ipv4_ipaddr="192.168.2.2"            # 修改openwrt后台地址(填0为关闭)
-export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
-export Op_name="OpenWrt-123"                # 修改主机名称为OpenWrt-123(填0为不作修改)
+Ipv4_ipaddr="192.168.150.2"            # 修改openwrt后台地址
+Netmask_netm="255.255.255.0"           # IPv4 子网掩码
+Op_name="༄ 目目+࿐"                   # 修改主机名称
+echo "CONFIG_IPV4_ADDR=\"$Ipv4_ipaddr\"" >> .config
+echo "CONFIG_NETMASK_NETM=\"$Netmask_netm\"" >> .config
+echo "CONFIG_OP_NAME=\"$Op_name\"" >> .config
+cat << EOF >> package/base-files/files/etc/uci-defaults/99-custom-settings
+uci set network.lan.ipaddr='$Ipv4_ipaddr'
+uci set network.lan.netmask='$Netmask_netm'
+uci set system.@system[0].hostname='$Op_name'
+uci commit
+EOF
 
-# 内核和系统分区大小(不是每个机型都可用)
-export Kernel_partition_size="0"            # 内核分区大小,每个机型默认值不一样 (填写您想要的数值,默认一般16,数值以MB计算，填0为不作修改),如果你不懂就填0
-export Rootfs_partition_size="0"            # 系统分区大小,每个机型默认值不一样 (填写您想要的数值,默认一般300左右,数值以MB计算，填0为不作修改),如果你不懂就填0
+# 内核和系统分区大小
+Kernel_partition_size="0"               # 内核分区大小 (MB)
+Rootfs_partition_size="0"               # 系统分区大小 (MB)
+echo "CONFIG_KERNEL_PARTITION_SIZE=\"$Kernel_partition_size\"" >> .config
+echo "CONFIG_ROOTFS_PARTITION_SIZE=\"$Rootfs_partition_size\"" >> .config
 
 # 默认主题设置
-export Mandatory_theme="argon"              # 将bootstrap替换您需要的主题为必选主题(可自行更改您要的,源码要带此主题就行,填写名称也要写对) (填写主题名称,填0为不作修改)
-export Default_theme="argon"                # 多主题时,选择某主题为默认第一主题 (填写主题名称,填0为不作修改)
+Mandatory_theme="argon"                 # 必选主题
+Default_theme="argon"                   # 默认第一主题
+echo "CONFIG_MANDATORY_THEME=\"$Mandatory_theme\"" >> .config
+echo "CONFIG_DEFAULT_THEME=\"$Default_theme\"" >> .config
 
 # 旁路由选项
-export Gateway_Settings="0"                 # 旁路由设置 IPv4 网关(填入您的网关IP为启用)(填0为不作修改)
-export DNS_Settings="0"                     # 旁路由设置 DNS(填入DNS，多个DNS要用空格分开)(填0为不作修改)
-export Broadcast_Ipv4="0"                   # 设置 IPv4 广播(填入您的IP为启用)(填0为不作修改)
-export Disable_DHCP="0"                     # 旁路由关闭DHCP功能(1为启用命令,填0为不作修改)
-export Disable_Bridge="0"                   # 旁路由去掉桥接模式(1为启用命令,填0为不作修改)
-export Create_Ipv6_Lan="0"                  # 爱快+OP双系统时,爱快接管IPV6,在OP创建IPV6的lan口接收IPV6信息(1为启用命令,填0为不作修改)
+Gateway_Settings="192.168.150.1"       # IPv4 网关
+DNS_Settings="192.168.151.2 223.5.5.5" # DNS 设置
+Broadcast_Ipv4="0"                      # IPv4 广播
+Disable_DHCP="1"                        # 关闭 DHCP
+Disable_Bridge="1"                      # 去掉桥接模式
+Create_Ipv6_Lan="0"                     # 创建 IPv6 LAN
+echo "CONFIG_GATEWAY_SETTINGS=\"$Gateway_Settings\"" >> .config
+echo "CONFIG_DNS_SETTINGS=\"$DNS_Settings\"" >> .config
+echo "CONFIG_BROADCAST_IPV4=\"$Broadcast_Ipv4\"" >> .config
+echo "CONFIG_DISABLE_DHCP=\"$Disable_DHCP\"" >> .config
+echo "CONFIG_DISABLE_BRIDGE=\"$Disable_Bridge\"" >> .config
+echo "CONFIG_CREATE_IPV6_LAN=\"$Create_Ipv6_Lan\"" >> .config
+cat << EOF >> package/base-files/files/etc/uci-defaults/99-custom-settings
+[ "$Gateway_Settings" != "0" ] && uci set network.lan.gateway='$Gateway_Settings'
+[ "$DNS_Settings" != "0" ] && uci set network.lan.dns='$DNS_Settings'
+[ "$Disable_DHCP" = "1" ] && uci set dhcp.lan.ignore='1'
+[ "$Disable_Bridge" = "1" ] && uci delete network.lan.type
+uci commit
+EOF
 
 # IPV6、IPV4 选择
-export Enable_IPV6_function="0"             # 编译IPV6固件(1为启用命令,填0为不作修改)(如果跟Create_Ipv6_Lan一起启用命令的话,Create_Ipv6_Lan命令会自动关闭)
-export Enable_IPV4_function="0"             # 编译IPV4固件(1为启用命令,填0为不作修改)(如果跟Enable_IPV6_function一起启用命令的话,此命令会自动关闭)
+Enable_IPV6_function="0"                # 启用 IPv6
+Enable_IPV4_function="0"                # 启用 IPv4
+if [ "$Enable_IPV6_function" = "1" ] && [ "$Enable_IPV4_function" = "1" ]; then
+    Enable_IPV4_function="0"            # 互斥处理
+fi
+[ "$Enable_IPV6_function" = "1" ] && Create_Ipv6_Lan="0"  # 互斥处理
+echo "CONFIG_ENABLE_IPV6_FUNCTION=\"$Enable_IPV6_function\"" >> .config
+echo "CONFIG_ENABLE_IPV4_FUNCTION=\"$Enable_IPV4_function\"" >> .config
 
-# 替换OpenClash的源码(默认master分支)
-export OpenClash_branch="0"                 # OpenClash的源码分别有【master分支】和【dev分支】(填0为使用master分支,填1为使用dev分支)
+# 替换 OpenClash 的源码
+OpenClash_branch="0"                    # 0 为 master 分支，1 为 dev 分支
+echo "CONFIG_OPENCLASH_BRANCH=\"$OpenClash_branch\"" >> .config
 
-# 个性签名,默认增加年月日[$(TZ=UTC-8 date "+%Y.%m.%d")]
-export Customized_Information="$(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为不作修改)
+# 个性签名
+Customized_Information="༄ 目目+࿐$(TZ=UTC-8 date '+%Y.%m.%d')"  # 个性签名
+echo "CONFIG_CUSTOMIZED_INFORMATION=\"$Customized_Information\"" >> .config
+[ -f package/lean/default-settings/files/zzz-default-settings ] && \
+    sed -i "s/OpenWrt /Custom Build $Customized_Information /g" package/lean/default-settings/files/zzz-default-settings
 
 # 更换固件内核
-export Replace_Kernel="0"                    # 更换内核版本,在对应源码的[target/linux/架构]查看patches-x.x,看看x.x有啥就有啥内核了(填入内核x.x版本号,填0为不作修改)
+Replace_Kernel="0"                      # 更换内核版本
+echo "CONFIG_REPLACE_KERNEL=\"$Replace_Kernel\"" >> .config
 
-# 设置免密码登录(个别源码本身就没密码的)
-export Password_free_login="1"               # 设置首次登录后台密码为空（进入openwrt后自行修改密码）(1为启用命令,填0为不作修改)
+# 设置免密码登录
+Password_free_login="1"                 # 首次登录无密码
+echo "CONFIG_PASSWORD_FREE_LOGIN=\"$Password_free_login\"" >> .config
+[ "$Password_free_login" = "1" ] && \
+    cat << EOF >> package/base-files/files/etc/uci-defaults/99-custom-settings
+uci delete system.@system[0].password
+uci commit system
+EOF
 
-# 增加AdGuardHome插件和核心
-export AdGuardHome_Core="0"                  # 编译固件时自动增加AdGuardHome插件和AdGuardHome插件核心,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为启用命令,填0为不作修改)
+# 增加 AdGuardHome 插件和核心
+AdGuardHome_Core="0"                    # 增加 AdGuardHome
+echo "CONFIG_ADGUARDHOME_CORE=\"$AdGuardHome_Core\"" >> .config
 
-# 开启NTFS格式盘挂载
-export Automatic_Mount_Settings="0"          # 编译时加入开启NTFS格式盘挂载的所需依赖(1为启用命令,填0为不作修改)
+# 禁用 NaiveProxy
+Disable_NaiveProxy="0"                  # 禁用 NaiveProxy
+echo "CONFIG_DISABLE_NAIVEPROXY=\"$Disable_NaiveProxy\"" >> .config
 
-# 去除网络共享(autosamba)
-export Disable_autosamba="0"                 # 去掉源码默认自选的luci-app-samba或luci-app-samba4(1为启用命令,填0为不作修改)
+# 开启 NTFS 格式盘挂载
+Automatic_Mount_Settings="0"            # NTFS 挂载支持
+echo "CONFIG_AUTOMATIC_MOUNT_SETTINGS=\"$Automatic_Mount_Settings\"" >> .config
 
-# 其他
-export Ttyd_account_free_login="0"           # 设置ttyd免密登录(1为启用命令,填0为不作修改)
-export Delete_unnecessary_items="0"          # 个别机型内一堆其他机型固件,删除其他机型的,只保留当前主机型固件(1为启用命令,填0为不作修改)
-export Disable_53_redirection="0"            # 删除DNS强制重定向53端口防火墙规则(个别源码本身不带此功能)(1为启用命令,填0为不作修改)
-export Cancel_running="0"                    # 取消路由器每天跑分任务(个别源码本身不带此功能)(1为启用命令,填0为不作修改)
+# 去除网络共享 (autosamba)
+Disable_autosamba="1"                   # 去掉 autosamba
+echo "CONFIG_DISABLE_AUTOSAMBA=\"$Disable_autosamba\"" >> .config
 
+# 其他设置
+Ttyd_account_free_login="0"             # ttyd 免密登录
+Delete_unnecessary_items="0"            # 删除多余固件
+Disable_53_redirection="0"              # 删除 DNS 53 重定向
+Cancel_running="0"                      # 取消跑分任务
+echo "CONFIG_TTYD_ACCOUNT_FREE_LOGIN=\"$Ttyd_account_free_login\"" >> .config
+echo "CONFIG_DELETE_UNNECESSARY_ITEMS=\"$Delete_unnecessary_items\"" >> .config
+echo "CONFIG_DISABLE_53_REDIRECTION=\"$Disable_53_redirection\"" >> .config
+echo "CONFIG_CANCEL_RUNNING=\"$Cancel_running\"" >> .config
+[ "$Ttyd_account_free_login" = "1" ] && \
+    cat << EOF >> package/base-files/files/etc/uci-defaults/99-custom-settings
+uci set ttyd.@ttyd[0].command='/bin/sh -l'
+uci commit ttyd
+EOF
 
-# 晶晨CPU系列打包固件设置(不懂请看说明)
-export amlogic_model="s905d"
-export amlogic_kernel="5.10.01_6.1.01"
-export auto_kernel="true"
-export rootfs_size="2560"
-export kernel_usage="stable"
-
-
+# 晶晨 CPU 系列打包固件设置
+amlogic_model="s905d"
+amlogic_kernel="5.10.01_6.1.01"
+auto_kernel="true"
+rootfs_size="2560"
+kernel_usage="stable"
+echo "CONFIG_AMLOGIC_MODEL=\"$amlogic_model\"" >> .config
+echo "CONFIG_AMLOGIC_KERNEL=\"$amlogic_kernel\"" >> .config
+echo "CONFIG_AUTO_KERNEL=\"$auto_kernel\"" >> .config
+echo "CONFIG_ROOTFS_SIZE=\"$rootfs_size\"" >> .config
+echo "CONFIG_KERNEL_USAGE=\"$kernel_usage\"" >> .config
 
 # 修改插件名字
-sed -i 's/"终端"/"TTYD"/g' `egrep "终端" -rl ./`
-sed -i 's/"网络存储"/"NAS"/g' `egrep "网络存储" -rl ./`
-sed -i 's/"实时流量监测"/"流量"/g' `egrep "实时流量监测" -rl ./`
-sed -i 's/"KMS 服务器"/"KMS激活"/g' `egrep "KMS 服务器" -rl ./`
-sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
-sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
-sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
-sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
-
+[ -d package ] && find package/ -type f -exec sed -i 's/"终端"/"终端TTYD"/g' {} +
+[ -d package ] && find package/ -type f -exec sed -i 's/"网络存储"/"NAS"/g' {} +
+[ -d package ] && find package/ -type f -exec sed -i 's/"实时流量监测"/"流量"/g' {} +
+[ -d package ] && find package/ -type f -exec sed -i 's/"KMS 服务器"/"KMS激活"/g' {} +
+[ -d package ] && find package/ -type f -exec sed -i 's/"USB 打印服务器"/"打印服务"/g' {} +
+[ -d package ] && find package/ -type f -exec sed -i 's/"Web 管理"/"Web管理"/g' {} +
+[ -d package ] && find package/ -type f -exec sed -i 's/"管理权"/"管理权"/g' {} +
+[ -d package ] && find package/ -type f -exec sed -i 's/"带宽监控"/"带宽监控"/g' {} +
 
 # 清理不需要的文件
 CLEAR_PATH="./clear_list.txt"
