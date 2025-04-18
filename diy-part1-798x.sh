b967a4c1717636b8485ff418a17c4bfd1c478267
shine85 <73932625+shine85@users.noreply.github.com>
2025-04-18 16:26:06 +0800

    Update diy-part1-798x.sh

--- diy-part1-798x.sh
+++ diy-part1-798x.sh
@@ -20,6 +20,8 @@ echo "========================="
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/redfrog999/openwrt-packages' feeds.conf.default
 sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
+#让led编译时可编译firewall4
+sed -i 's/+firewall/+uci-firewall/g' feeds/luci/applications/luci-app-firewall/Makefile
 # sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 # sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
