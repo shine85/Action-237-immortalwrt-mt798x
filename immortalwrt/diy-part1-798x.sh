f2a31c48adf8e2143cc38ec8c40762f1a2376624
shine85 <73932625+shine85@users.noreply.github.com>
2025-04-22 10:29:30 +0800

    更新 diy-part1-798x.sh

--- immortalwrt/diy-part1-798x.sh
+++ immortalwrt/diy-part1-798x.sh
@@ -20,8 +20,9 @@ echo "========================="
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/redfrog999/openwrt-packages' feeds.conf.default
 sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' feeds.conf.default
+echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git;main' >>feeds.conf.default
+sed -i '2i src-git homeproxy https://github.com/immortalwrt/homeproxy.git;main' feeds.conf.default
 src-git homeproxy https://github.com/immortalwrt/homeproxy.git;main package/package/homeproxy
-# sed -i '$a src-git homeproxy https://github.com/immortalwrt/homeproxy.git;main' feeds.conf.default
 # sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 # sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
