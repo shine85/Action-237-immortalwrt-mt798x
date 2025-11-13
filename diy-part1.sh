a75d19542bf911cc34546f91ba83b91678eee9cc
shine85 <73932625+shine85@users.noreply.github.com>
2025-11-13 11:05:35 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages.git;main package/passwall-packages
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 # 科学passwall
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 # 科学passwall2
