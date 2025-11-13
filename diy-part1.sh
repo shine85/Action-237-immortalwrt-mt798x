bb7b9c5aba4ec285ec118cac07ae585214988dfa
shine85 <73932625+shine85@users.noreply.github.com>
2025-11-13 10:43:22 +0800

    +

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,15 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+src-git passwall https://github.com/xiaorouji/openwrt-passwall-packages.git;main package/passwall-packages
+# 科学passwall
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main package/luci-app-passwall2-packages
+# 科学passwall2
+
+src-git homeproxy https://github.com/immortalwrt/homeproxy.git package/homeproxy
+#  lede添加不了
+src-git advancedplus https://github.com/sirpdboy/luci-app-advancedplus.git package/advancedplus  
+#进阶设置   lede添加不了
+src-git kucat https://github.com/sirpdboy/luci-theme-kucat.git package/kucat  
+#主题
+src-git filemanager https://github.com/sbwml/luci-app-filemanager.git package/filemanager 
